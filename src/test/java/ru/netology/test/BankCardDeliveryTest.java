637236ec49e4ab6adbd041c922a63ea78388612a
Sveta Golubeva <veta.golubeva@gmail.com>
2023-07-03 16:47:09 +0300

    Plan message

--- src/test/java/ru/netology/test/BankCardDeliveryTest.java
+++ src/test/java/ru/netology/test/BankCardDeliveryTest.java
@@ -39,7 +39,7 @@ class BankCardDeliveryTest {
         $(byText("Запланировать")).click();
         $(byText("Успешно!")).shouldBe(visible, Duration.ofSeconds(10));
 
-        $("[data-test-id=success-notification] .notification__title").shouldHave(text("Успешно!")).shouldBe(visible, Duration.ofSeconds(10));
+        $("[data-test-id=success-notification] .notification__content").shouldHave(text("Встреча успешно запланирована на " + firstMeetingDate)).shouldBe(visible, Duration.ofSeconds(10));
 
         $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         $("[data-test-id=date] input").setValue(secondMeetingDate);
@@ -48,7 +48,7 @@ class BankCardDeliveryTest {
         $("[data-test-id=replan-notification]").shouldHave(text("У вас уже запланирована встреча на другую дату. Перепланировать?")).shouldBe(visible, Duration.ofSeconds(10));
         $("[data-test-id=replan-notification] button").click();
 
-        $("[data-test-id=success-notification] .notification__title").shouldHave(text("Успешно!")).shouldBe(visible, Duration.ofSeconds(10));
+        $("[data-test-id=success-notification] .notification__content").shouldHave(text("Встреча успешно запланирована на " + secondMeetingDate)).shouldBe(visible, Duration.ofSeconds(10));
     }
 
     @Test
